f23626a49063917d7c602fdb0e9e3375cc8aee62
Jonmercado1 <159264254+Jonmercado1@users.noreply.github.com>
2025-12-13 09:42:49 -0500

    added a strawberry to the custom items and used the food builder to give it some properties, kinda weird to add to the modItems place, just a little different

--- src/main/java/net/jon/tutorialmod/item/ModItems.java
+++ src/main/java/net/jon/tutorialmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.jon.tutorialmod.item;
 
 import net.jon.tutorialmod.TutorialMod;
 import net.jon.tutorialmod.item.custom.MetalDetectorItem;
+import net.jon.tutorialmod.item.custom.ModFoods;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,7 +18,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire", () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector", () -> new MetalDetectorItem(new Item.Properties()
+            .durability(100)));
+
+    public static final RegistryObject<Item> STRAWBERRY = ITEMS.register("strawberry", () -> new Item(new Item.Properties().food(ModFoods.STRAWBERRY)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/net/jon/tutorialmod/item/custom/ModFoods.java
@@ -0,0 +1,10 @@
+package net.jon.tutorialmod.item.custom;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+    public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
+            .saturationMod(8.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
+}
