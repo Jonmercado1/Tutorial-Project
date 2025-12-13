f27275c4e9f970a47fdbe05ff254524614591c2b
Jonmercado1 <159264254+Jonmercado1@users.noreply.github.com>
2025-12-13 09:26:14 -0500

    Created a sound block when right clicked creates a sound.

--- src/main/java/net/jon/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/jon/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.jon.tutorialmod.block;
 
 import net.jon.tutorialmod.TutorialMod;
+import net.jon.tutorialmod.block.custom.SoundBlock;
 import net.jon.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
@@ -26,6 +27,8 @@ public class ModBlocks {
     public static final RegistryObject<Block> RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE)));
 
+    public static final RegistryObject<Block> SOUND_BLOCK = registerBlock("sound_block",
+            () -> new SoundBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)));
     //ores
 
     public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
--- src/main/java/net/jon/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/jon/tutorialmod/item/ModCreativeModeTabs.java
@@ -26,6 +26,7 @@ public class ModCreativeModeTabs {
 
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
+                        pOutput.accept(ModBlocks.SOUND_BLOCK.get());
 
                         pOutput.accept(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
                         pOutput.accept(ModBlocks.NETHER_SAPPHIRE_ORE.get());
